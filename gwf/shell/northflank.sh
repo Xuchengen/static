75c81dc70003093d61f32c0a5c44572e38fbb6b4
徐承恩 <771247770@qq.com>
2024-08-22 01:34:25 +0800

    更新脚本

--- gwf/shell/northflank.sh
+++ gwf/shell/northflank.sh
@@ -7,11 +7,14 @@ apk add openssh
 chmod 600 /root/.ssh/id_ed25519
 chmod 644 /root/.ssh/id_ed25519.pub
 
+# 设置git全局邮箱
+git config --global user.email "$GITHUB_EMAIL"
+
 # 进入root目录
 cd /root
 
 # 克隆代码
-git clone -b us git@github.com:Xuchengen/northflank.git
+yes | head -n 1 | git clone -b us git@github.com:Xuchengen/northflank.git
 
 # 启动哪吒agent
 /root/northflank/nezha/nezha-agent.sh &
