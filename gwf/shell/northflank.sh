8a3b793171e3c1c72520d099ef80f307649806df
徐承恩 <771247770@qq.com>
2024-08-22 01:15:35 +0800

    更新代码

--- gwf/shell/northflank.sh
+++ gwf/shell/northflank.sh
@@ -7,7 +7,13 @@ apk add openssh
 chmod 600 /root/.ssh/id_ed25519
 chmod 644 /root/.ssh/id_ed25519.pub
 
+# 进入root目录
+cd /root
+
+# 克隆代码
+git clone -b us git@github.com:Xuchengen/northflank.git
+
 # 启动哪吒agent
-# /root/northflank/nezha/nezha-agent.sh &
+/root/northflank/nezha/nezha-agent.sh &
 
 tail -f /dev/null
