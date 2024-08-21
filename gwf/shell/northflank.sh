24bcb8b613f3639f755497d5b31ae85021682f2a
徐承恩 <771247770@qq.com>
2024-08-22 00:56:39 +0800

    更新脚本

--- gwf/shell/northflank.sh
+++ gwf/shell/northflank.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-chmod 600 /root/.ssh/id_rsa
-chmod 644 /root/.ssh/id_rsa.pub
-
 # 安装openssh
 apk add openssh
+
+# 变更文件权限
+chmod 600 /root/.ssh/id_ed25519
+chmod 644 /root/.ssh/id_ed25519.pub
 
 # 启动哪吒agent
 # /root/northflank/nezha/nezha-agent.sh &
