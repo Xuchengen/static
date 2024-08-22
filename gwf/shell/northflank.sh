0c04a13ea48524519b4a0533f78cd38171bce65a
徐承恩 <771247770@qq.com>
2024-08-22 19:45:49 +0800

    更新脚本

--- gwf/shell/northflank.sh
+++ gwf/shell/northflank.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-# 安装openssh
-apk add openssh htop
+# 安装必要软件包
+apk add bash openssh htop tzdata
 
-# 变更文件权限
-chmod 600 /root/.ssh/id_ed25519
-chmod 644 /root/.ssh/id_ed25519.pub
+# 设置中国时区
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+echo "Asia/Shanghai" > /etc/timezone
 
 # 设置git全局邮箱
 git config --global user.email "$GITHUB_EMAIL"
+
+# 设置Github私钥文件权限
+chmod 600 /root/.ssh/id_ed25519
+chmod 644 /root/.ssh/id_ed25519.pub
 
 # 进入root目录
 cd /root
