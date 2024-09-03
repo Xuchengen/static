2845b3eb462184a3d58045be52e39ee94fc2dc1a
徐承恩 <771247770@qq.com>
2024-09-03 11:44:19 +0800

    更新脚本

--- scripts/binary/nezha-agent.sh
+++ scripts/binary/nezha-agent.sh
@@ -5,5 +5,5 @@ URL="https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_linux
 
 if curl -s -o /dev/null -I -L -w "%{http_code}" "$URL" | grep -q '^2'; then
   echo "远程 nezha-agent_linux_amd64.zip 文件存在，准备下载"
-  curl -sL -o /usr/local/src/binary/$FILE_NAME "$URL"
+  curl -sL -o ~/dist_binary/$FILE_NAME "$URL"
 fi
